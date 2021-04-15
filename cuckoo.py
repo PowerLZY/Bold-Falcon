b345599648a1d5974fb752cd2b3c8d20df3269fb
PowerLZY <3289218653@qq.com>
2021-04-15 18:57:44 +0800

    test cuckoo -d

--- cuckoo.py
+++ cuckoo.py
@@ -5,5 +5,4 @@ import sys
 from cuckoo.main import main
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
-    sys.argv[0] = sys.argv[0] + '-d' # Enable verbose logging
     sys.exit(main())
