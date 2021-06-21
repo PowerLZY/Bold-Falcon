02d1e49e01824aa339eeb118c562a716ba8bf283
PowerLZY <3289218653@qq.com>
2021-06-21 21:45:23 +0800

    dele old cuckoo

--- cuckoo/distributed/exception.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2014-2016 Cuckoo Foundation.
-# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
-# See the file 'docs/LICENSE' for copying permission.
-
-class InvalidReport(Exception):
-    pass
-
-class InvalidPcap(Exception):
-    pass
