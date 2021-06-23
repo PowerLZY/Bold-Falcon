0e51bee428d8c75b038b7155f20dbe07fcc1b9db
PowerLZY <3289218653@qq.com>
2021-06-23 12:25:19 +0800

    ml.load_features(features_dict) : loaded into ML class the data is reformated into Pandas DataFrame object

--- examples/detectiontest.py
+++ examples/detectiontest.py
@@ -6,65 +6,6 @@ from modules.detection.loader import Loader
 from modules.processing.cuckooml import ML
 from pprint import pprint
 
-relabelled = True  # False
-read_from = "../sample_data/sample"
-save_in = "../sample_data/dict"
-
-"""if your data was not produced with one of the latest cuckoo versions there might be a hance that you're missing data labels; in this case you need to run labeling process independently; to do so please follow the guideline below.)
-If you have just got the data from the above code cell you need to execute the code below"""
-if relabelled:
-    import json
-    import os
-    import sys
-    from lib.cuckoo.common.virustotal import VirusTotalAPI
-
-    if not os.path.exists(save_in):
-        os.makedirs(save_in)
-    vt = VirusTotalAPI("", 0, 0)
-
-    for f in os.listdir(read_from):
-        with open(read_from + "/" + f, "r") as malware_report:
-            try:
-                report = json.load(malware_report)
-            except ValueError, error:
-                print >> sys.stderr, "Could not load file;", malware_report, "is not a valid JSON file."
-                print >> sys.stderr, "Exception: %s" % str(error)
-                print >> sys.stderr, "Moving on to the next file..."
-                continue
-
-            if report["virustotal"].get("scans") is not None:
-                report["virustotal"]["normalized"] = {
-                    "cve": "",
-                    "platform": "",
-                    "metatype": "",
-                    "type": "",
-                    "family": ""
-                }
-
-                norm_lower = {
-                    "cve": [],
-                    "platform": [],
-                    "metatype": [],
-                    "type": [],
-                    "family": [],
-                }
-
-                for vendor in report["virustotal"]["scans"]:
-                    report["virustotal"]["scans"][vendor]["normalized"] = \
-                        vt.normalize(report["virustotal"]["scans"][vendor]["result"])
-
-                    for label_type in report["virustotal"]["scans"][vendor]["normalized"]:
-                        norm_lower[label_type] += \
-                            report["virustotal"]["scans"][vendor]["normalized"][label_type]
-
-                labeller = Instance()
-                for label_type in norm_lower:
-                    labeller.label_sample(norm_lower[label_type])
-                    report["virustotal"]["normalized"][label_type] = labeller.label
-
-            with open(save_in + "/" + f, "w") as malware_report_updated: # save_in : '../sample_data/dict'
-                json.dump(report, malware_report_updated)
-
 # The first stage is to load the data from the directory holding all the JSONs
 loader = Loader()
 loader.load_binaries("../sample_data/dict")
@@ -72,4 +13,30 @@ loader.load_binaries("../sample_data/dict")
 # Then we extract all the relevant information from the loaded samples.
 simple_features_dict = loader.get_simple_features()
 features_dict = loader.get_features()
+"""
+{simple_features_dict['165']:
+u'injection_runpe': u'Executed a process and injected code into it, probably while unpacking', 
+u'worm_renocide': u'Creates known Renocide Worm files, registry keys and/or mutexes', 
+u'pe_features': u'The executable has PE anomalies (could be a false positive)', 
+u'raises_exception': u'One or more processes crashed', 
+u'dumped_buffer': u'One or more potentially interesting buffers were extracted, these generally contain injected code, configuration data, etc.', 
+u'antisandbox_productid': u'Retrieves Windows ProductID, probably to fingerprint the sandbox',
+u'antivirus_virustotal': u'File has been identified by at least one AntiVirus on VirusTotal as malicious', 
+u'allocates_rwx': u'Allocates read-write-execute memory (usually to unpack itself)',
+ u'packer_upx': u'The executable is compressed using UPX'}
+"""
 labels_dict = loader.get_labels()
+
+# Now that all the needed information are at hand we create a cuckooml Machine Learning instance
+# and inject therein all these information.
+# ml = ML(context="notebook")
+ml = ML()
+ml.load_simple_features(simple_features_dict)
+ml.load_features(features_dict)
+ml.load_labels(labels_dict)
+
+# Once loaded into ML class the data is reformated into Pandas DataFrame object
+# therefore it is easy to manipulate and use it with variety of machine learning algorithms.
+simple_features = ml.simple_features
+features = ml.features
+labels = ml.labels
