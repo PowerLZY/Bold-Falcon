958caa57b9232102a78e97743815d8a099ebde07
PowerLZY <3289218653@qq.com>
2021-07-12 12:33:19 +0800

    update

--- utils/community.py
+++ utils/community.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # Copyright (C) 2010-2013 Claudio Guarnieri.
 # Copyright (C) 2014-2016 Cuckoo Foundation.
+# Copyright (C) 2020-2021 PowerLZY.
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
